7e8af6fd2a0d24e8d3a38105ddc4770e0ca29e75
Max Kraev <mk@setmysails.com>
2018-04-02 09:15:36 +0500

    Quizzler project

--- 04_Quizzler/Quizzler/ViewController.swift
+++ 04_Quizzler/Quizzler/ViewController.swift
@@ -8,8 +8,10 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    //Place your instance variables here
-    
+    let allQuestions = QuestionBank()
+    var pickedAnswer = false
+    var questionNumber = 0
+    var score = 0
     
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
@@ -18,12 +20,57 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        questionLabel.text = allQuestions.list[questionNumber].questionText
+        updateUI()
     }
 
-
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        checkAnswer()
+        nextQuestion()
+        updateUI()
+    }
+    
+    func updateUI() {
+        scoreLabel.text = "Score: \(score)"
+        progressLabel.text = "\(questionNumber + 1)/13"
+        progressBar.frame.size.width = view.frame.size.width / 13 * CGFloat(questionNumber + 1)
+    }
+    
+    func nextQuestion() {
+        if questionNumber < allQuestions.list.count - 1 {
+            questionNumber += 1
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        } else {
+            let ac = UIAlertController(title: "The End", message: "You've made it to the end \n Your score is \(score)", preferredStyle: .alert)
+            let restartAction = UIAlertAction(title: "Restart", style: .default) { _ in
+                self.restart()
+            }
+            ac.addAction(restartAction)
+            
+            present(ac, animated: true)
+        }
+
+    }
+    
+    func checkAnswer() {
+        if pickedAnswer == allQuestions.list[questionNumber].answer {
+            score += 1
+            updateUI()
+        }
+    }
+    
+    func restart()   {
+        score = 0
+        questionNumber = 0
+        updateUI()
+        
+        questionLabel.text = allQuestions.list[questionNumber].questionText
     }
     
 }
