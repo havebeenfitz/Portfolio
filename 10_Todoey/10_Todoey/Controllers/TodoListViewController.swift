ce7241bb0f2706d81439ac16ea6437407ceb2f31
Max Kraev <mk@setmysails.com>
2018-04-05 22:41:56 +0500

    Complete saving data with NSCoder

--- 10_Todoey/10_Todoey/Controllers/TodoListViewController.swift
+++ 10_Todoey/10_Todoey/Controllers/TodoListViewController.swift
@@ -11,18 +11,19 @@ import UIKit
 class TodoListViewController: UITableViewController {
     
     var itemArray = [ToDoItem]()
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
+    
     override func viewDidLoad() {
+
         super.viewDidLoad()
         let item = ToDoItem()
         item.title = "Add Item"
         item.subTitile = "or not"
         itemArray.append(item)
         
-        if let items = defaults.object(forKey: "ToDoListArray") as? [ToDoItem] {
-            itemArray = items
-        }
+        loadItems()
+        
     }
     
     // MARK: - Table view data source
@@ -53,7 +54,8 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
-
+        saveItems()
+        
         tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
         
         tableView.deselectRow(at: indexPath, animated: true)
@@ -76,13 +78,15 @@ class TodoListViewController: UITableViewController {
         let addItem = UIAlertAction(title: "Add", style: .default) { action in
             
             if textField.text! != "" {
-                
+
                 let newItem = ToDoItem()
                 newItem.title = textField.text!
                 self.itemArray.append(newItem)
                 
-                self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+                self.saveItems()
+                
                 self.tableView.reloadData()
+                
             } else {
                 let ac = UIAlertController(title: "Empty entry", message: "Please type something", preferredStyle: .alert)
                 let okAction = UIAlertAction(title: "Ok", style: .default, handler: nil)
@@ -93,6 +97,33 @@ class TodoListViewController: UITableViewController {
         
         ac.addAction(addItem)
         present(ac, animated: true)
+        
+    }
+    // MARK: - Save & Load Items NSCoder
+    
+    func saveItems() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding \(error)")
+        }
+    }
+    
+    func loadItems() {
+       
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([ToDoItem].self, from: data)
+            } catch {
+                print(error)
+            }
+            
+        }
+        
         
     }
     
